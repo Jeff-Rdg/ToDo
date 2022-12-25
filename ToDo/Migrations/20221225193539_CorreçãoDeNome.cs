fb959aae61a0e8ba1e73f8da561df4a657b0eee7
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2022-12-25 16:36:39 -0300

    Correção de propriedade da tabela

---
+++ ToDo/Migrations/20221225193539_CorreçãoDeNome.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorreçãoDeNome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Tasks",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Tasks",
+                newName: "Name");
+        }
+    }
+}
--- ToDo/Models/Tasks.cs
+++ ToDo/Models/Tasks.cs
@@ -3,7 +3,7 @@
     public class Tasks
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
         public EnumStatusTasks Status { get; set; }
