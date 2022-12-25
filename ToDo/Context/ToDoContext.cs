b31c31b60f1c68e2289220c58e15aad04c0fa069
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2022-12-25 16:23:36 -0300

    Adicionando Contexto e dependencias com EntityFramework

---
+++ ToDo/Context/ToDoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Context
+{
+    public class ToDoContext :DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tasks> Tasks { get; set; }
+    }
+}
